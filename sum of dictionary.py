9109a298a97328382cf9c2356b938f958f7bb842
sushmakandra <77783700+sushmakandra@users.noreply.github.com>
2021-01-21 17:40:03 +0530

    Add files via upload

---
+++ sum of dictionary.py
@@ -0,0 +1,11 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> dictonary={'a':1,'b':2,'c':3,'d':4}
+>>> print('attendence dictonary of all the divisions...')
+attendence dictonary of all the divisions...
+>>> print(dictonary)
+{'a': 1, 'b': 2, 'c': 3, 'd': 4}
+>>> tot=sum(dictonary.values())
+>>> print('total attendance:',tot)
+total attendance: 10
+>>> 
